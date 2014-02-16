170291422853b4e649cd4a7d0857d403769c42ac
awachner <awachner@mail.uni-mainheim.de>
2014-02-16 17:14:24 +0100

    database connection class created and libraries added.

--- GoMobileTest/bin/MainTest-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/tendlich/git/goMobile/GoMobileTest/bin/MainTest-debug-unaligned.apk : \
-/Users/tendlich/git/goMobile/GoMobileTest/bin/MainTest.ap_ \
-/Users/tendlich/git/goMobile/GoMobileTest/bin/classes.dex \
-/Users/tendlich/git/goMobile/GoMobileTest/libs/android-support-v4.jar \
--- GoMobileTest/bin/classes.dex.d
+++
@@ -1,8 +0,0 @@
-/Users/tendlich/git/goMobile/GoMobileTest/bin/classes.dex : \
-/Users/tendlich/git/goMobile/GoMobileTest/bin/classes/com/gomobile/test/BuildConfig.class \
-/Users/tendlich/git/goMobile/GoMobileTest/bin/classes/com/gomobile/test/R$attr.class \
-/Users/tendlich/git/goMobile/GoMobileTest/bin/classes/com/gomobile/test/R$drawable.class \
-/Users/tendlich/git/goMobile/GoMobileTest/bin/classes/com/gomobile/test/R$string.class \
-/Users/tendlich/git/goMobile/GoMobileTest/bin/classes/com/gomobile/test/R.class \
-/Users/tendlich/git/goMobile/GoMobileTest/bin/classes/com/gomobile/test/ShowTechSpecsActivityTest.class \
-/Users/tendlich/git/goMobile/GoMobileTest/bin/dexedLibs/android-support-v4-feed5fc6dc40227f8d7a7ebf30a8ed77.jar \
