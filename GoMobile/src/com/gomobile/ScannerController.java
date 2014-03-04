a228c34e7a0a07c764f18a6ebce9311671bd717a
tendlich <tendlich@mail.uni-mannheim.de>
2014-03-04 11:00:54 +0100

    added history

--- GoMobile/src/com/gomobile/ScannerController.java
+++ GoMobile/src/com/gomobile/ScannerController.java
@@ -1,12 +1,20 @@
 package com.gomobile;
 
+import java.util.ArrayList;
+
 import com.gomobile.scanner.model.Component;
 
 public class ScannerController {
 		
 	private Component componentInUse;
+	private int listSize = 10;
+	private ArrayList<Component> history = new ArrayList<Component>();
 	
 	public void setComponentInUse(Component component){
+		if(history.size() == listSize){
+			history.remove(9);
+		}
+		history.add(componentInUse);
 		this.componentInUse = component;
 	}
 	
