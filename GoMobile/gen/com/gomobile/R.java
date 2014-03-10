83d32698ec006e77ea128f35ce947e2655e39844
tendlich <tendlich@mail.uni-mannheim.de>
2014-03-10 11:56:52 +0100

    changed layout

--- GoMobile/gen/com/gomobile/R.java
+++ GoMobile/gen/com/gomobile/R.java
@@ -984,7 +984,9 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
-        public static final int LinearLayout2=0x7f050040;
+        public static final int LinearLayout2=0x7f050042;
+        public static final int TextView01=0x7f050041;
+        public static final int TextView02=0x7f05003e;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -993,12 +995,12 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
         public static final int action_context_bar=0x7f05001d;
-        public static final int action_detail_view=0x7f050043;
+        public static final int action_detail_view=0x7f050045;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_scanner=0x7f050042;
-        public static final int action_settings=0x7f050041;
+        public static final int action_scanner=0x7f050044;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1045,8 +1047,8 @@ containing a value of this type.
         public static final int tabMode=0x7f050002;
         public static final int textView2=0x7f05003c;
         public static final int textView3=0x7f05003d;
-        public static final int textViewName=0x7f05003e;
-        public static final int textViewPrice=0x7f05003f;
+        public static final int textViewName=0x7f05003f;
+        public static final int textViewPrice=0x7f050040;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
